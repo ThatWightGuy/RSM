d4322ba17a2a8eecd70c99511b5bff2e6e66a3b5
ThatWightGuy <tman.wight@gmail.com>
2019-07-26 14:54:22 -0700

    first commit

---
+++ RSM/rsm.py
@@ -0,0 +1,5 @@
+import xml.etree.ElementTree as ET
+
+class RSM:
+    def __init__(self, file):
+        self.file = file
---
+++ RSM/testinterface.py
@@ -0,0 +1,5 @@
+import rsm
+
+if __name__ == '__main__':
+    with open("test.rsm", "r+") as test:
+        r = rsm.RSM(test)
