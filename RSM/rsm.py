cd188812734a521fdb9bc4bd2d1fc05a991d3caf
ThatWightGuy <tman.wight@gmail.com>
2019-08-06 14:44:31 -0700

    Started working on the parser and changed the name to the test.rsm file

--- RSM/rsm.py
+++ RSM/rsm.py
@@ -1,7 +1,46 @@
 import xml.etree.ElementTree as ET
 
+class TEST:
+    def __str__(self):
+        return "POOP"
+
 class RSM:
     def __init__(self, file):
-        self.file = file
+        self.file = self.__check_init_res(file)
+        self.meta = {"NUM_PAGES": self.__find_num_pages(),
+                     "NUM_EDU": 0,
+                     "NUM_EMP": 0,
+                     "NUM_SKILLS": 0,
+                     "NUM_CERTS": 0,
+                     "NUM_PROJ": 0,
+                     }
+
+        # self.data = TODO: write "find data" function(s). This is gonna be a dictionary that holds both the <header> and <info> info.
+
+    # Exceptions:
+    class InitResException(Exception):
+        """ Raised When Initial Parent Tag is not 'res' """
+        def __str__(self):
+            return "Initial parent tag is not 'res'"
+
+    # Data Classes:
+    # TODO: write class for
+
+    def __check_init_res(self, file):
         et = ET.parse(file)
-        print(et.getroot().tag)
+        root = et.getroot()
+
+        if root.tag.lower() == "res":
+            return et
+        else:
+            raise self.InitResException()
+
+    def __find_num_pages(self):
+        root = self.file.getroot()
+        num = 0
+
+        for _ in root.iter('page'):
+            num += 1
+
+        return num
+
--- RSM/testinterface.py
+++ RSM/testinterface.py
@@ -1,5 +1,6 @@
 import rsm
 
 if __name__ == '__main__':
-    with open("RSMFiles/test.rsm", "r+") as test:
+    with open("RSMFiles/TravisWight.rsm", "r+") as test:
         r = rsm.RSM(test)
+        print(r.meta["NUM_PAGES"])
